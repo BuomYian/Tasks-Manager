e023121df686e76286b9efd1c228d266985e501f
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-09-10 20:30:06 +0300

    change from npm to yarn

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-  return <div>Tasks Manager</div>;
+  return <div>Hello There</div>;
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 
-const port = 5000;
+const port = 4000;
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
